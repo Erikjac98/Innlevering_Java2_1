503a1f47c22dd568a4c46105520806d31ad7fdcb
Erikjac98 <jaceri17@student.westerdals.no>
2018-10-31 19:06:14 +0100

    Test2 innlevering

--- src/main/java/no/kristiania/pgr200/database/DatabaseMain.java
+++ src/main/java/no/kristiania/pgr200/database/DatabaseMain.java
@@ -1,7 +1,22 @@
 package no.kristiania.pgr200.database;
 
+import javax.swing.plaf.nimbus.State;
+import java.io.IOException;
+import java.sql.*;
 
 public class DatabaseMain {
+    public static void createTable(){
 
+        String url = "jdbc:postgresql://localhost:5432/postgres";
 
+        String sql = "CREATE TABLE Conference (\n" + "conference_id integer PRIMARY KEY, \n" +
+                "days integer NOT NULL, \n" + "date String NOT NULL \n" + "talk_id integer FOREIGN KEY\n" +
+                ");";
+        try (Connection conn = DriverManager.getConnection(url);
+        Statement statement = conn.createStatement()){
+            statement.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
 }
