baaf2b2a255c89cb945a032728f0f95df3385bbc
TheRealJohnD0e <bngyllenhaaljohansen@gmail.com>
2018-10-31 19:50:45 +0100

    Added .iml file

--- src/main/java/no/kristiania/pgr200/database/DatabaseMain.java
+++ src/main/java/no/kristiania/pgr200/database/DatabaseMain.java
@@ -1,16 +1,23 @@
 package no.kristiania.pgr200.database;
 
+import org.postgresql.ds.PGPoolingDataSource;
+
 import javax.swing.plaf.nimbus.State;
 import java.io.IOException;
 import java.sql.*;
+import javax.sql.DataSource;
+
+
 
 public class DatabaseMain {
     public static void createTable(){
 
+        DataSource dataSource = createDataSource();
+
         String url = "jdbc:postgresql://localhost:5432/postgres";
 
         String sql = "CREATE TABLE Conference (\n" + "conference_id integer PRIMARY KEY, \n" +
-                "days integer NOT NULL, \n" + "date String NOT NULL \n" + "talk_id integer FOREIGN KEY\n" +
+                "days integer NOT NULL, \n" + "date integer NOT NULL \n" +
                 ");";
         try (Connection conn = DriverManager.getConnection(url)) {
             try (Statement statement = conn.createStatement()) {
@@ -23,5 +30,14 @@ public class DatabaseMain {
 
     public static void main(String[] args){
         createTable();
+    }
+
+    public static DataSource createDataSource(){
+        PGPoolingDataSource dataSource = new PGPoolingDataSource();
+        dataSource.setUrl("jdbc:postgresql://localhost:5432/postgres");
+        dataSource.setUser("gyllenhaal");
+        dataSource.setPassword("admin");
+
+        return dataSource;
     }
 }
