4e3156b53f3a18458d17573655db92422b83ab49
Erikjac98 <jaceri17@student.westerdals.no>
2018-11-13 22:44:56 +0100

    Added port to RootHandler

--- src/main/java/no/kristiania/pgr200/database/DatabaseMain.java
+++ src/main/java/no/kristiania/pgr200/database/DatabaseMain.java
@@ -3,21 +3,56 @@ package no.kristiania.pgr200.database;
 import javax.swing.plaf.nimbus.State;
 import java.io.IOException;
 import java.sql.*;
+import java.util.Scanner;
 
 public class DatabaseMain {
 
-    public static void createTable(){
 
-        String url = "jdbc:postgresql://localhost:5432/postgres";
+    public static void createTable(String url, String sql){
 
-        String sql = "CREATE TABLE Conference (\n" + "conference_id integer PRIMARY KEY, \n" +
-                "days integer NOT NULL, \n" + "date String NOT NULL \n" + "talk_id integer FOREIGN KEY\n" +
-                ");";
         try (Connection conn = DriverManager.getConnection(url);
         Statement statement = conn.createStatement()){
             statement.execute(sql);
         } catch (SQLException e) {
             System.out.println(e.getMessage());
         }
+    }
+
+    public static void addTalk(String url) {
+
+        Scanner scanner = new Scanner(System.in);
+        String talkTitle = scanner.nextLine();
+        int talkID = scanner.nextInt();
+        String talkDescription = scanner.nextLine();
+        String talkTopic = scanner.nextLine();
+        int conferenceID = scanner.nextInt();
+
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement statement = conn.createStatement()){
+            statement.execute("INSERT INTO TABLE talks (talk_title, talk_id, talk_description, talk_topic, conference_id) \n"
+                                    + "VALUES \n("  + talkID + ", " + talkTitle + ", " + talkDescription + ", " + talkTopic + ", "
+                                    + conferenceID + ");");
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+    public static void showTalks(String url){
+
+        String sql = "SELECT * FROM talks";
+
+        try (Connection conn = DriverManager.getConnection(url);
+             Statement statement = conn.createStatement()){
+            statement.execute(sql);
+        } catch (SQLException e) {
+            System.out.println(e.getMessage());
+        }
+
+
+    }
+
+    public static void main(String[] args){
+
     }
 }
